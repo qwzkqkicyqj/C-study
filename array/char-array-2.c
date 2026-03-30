3354953e846bb35685d2c35c574542597e15a359
byeonwansu <qwzkqkicyqj@naver.com>
2026-03-30 15:17:03 +0900

    문자 배열 실습2

---
+++ array/char-array-2.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+
+int main() {
+	char fuirts[3][21];
+	for (int i = 0; i <= 2; i++) {
+		printf("과일 이름을 입력하세요.: ");
+		scanf("%s", fuirts[i]);
+	}
+	for (int j = 0; j <= 2; j++) {
+		printf("%d번쩨 과일: ", j+1);
+		printf("%s\n", fuirts[j]);
+	}
+
+	return 0;
+}
