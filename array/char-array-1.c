8137fc9c49b48d056c638b957866daa0f8c4a4c3
byeonwansu <qwzkqkicyqj@naver.com>
2026-03-30 14:47:05 +0900

    문자 배열 실습

---
+++ array/char-array-1.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	char str[30] = "C language"; //문자배열 30자
+	int i = 0; //배열이 0부터 시작
+
+	while (str[i] != '\0') { //만족할때 반복
+		i++;				 //!=: 다르다, \0: null -> null이 아니라면
+							 //문자열 내용 끝나는 지점 다음에 NULL이 자동으로 들어감.
+							 //따라서 널 값을 고려히여 배열의 크기를 짜야 함.
+	}
+	printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+
+	return 0;
+	}
