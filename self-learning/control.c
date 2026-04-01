1008116f0f5a596b92eeb42a893135c29ce70aed
byeonwansu <qwzkqkicyqj@naver.com>
2026-04-01 15:34:51 +0900

    4번 문제

---
+++ self-learning/control.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main() {
+	int cnt = 0;
+	for (int i = 1; ; i++) {
+		if (i > 10) {
+			break;
+		}
+		if (i % 3 == 0) {
+			cnt++;
+		}
+	}
+	printf("3의 배수 개수 : %d\n", cnt); //3의 배수 개수 : 3
+}
