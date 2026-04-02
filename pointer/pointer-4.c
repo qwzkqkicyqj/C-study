2753b562a27dad496ed630e3bd108d269c228d21
byeonwansu <qwzkqkicyqj@naver.com>
2026-04-02 15:49:49 +0900

    포인터 실습 4

---
+++ pointer/pointer-4.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int count_even(int a[10]); //10개의 배열(0~9)
+void Printcount(int a);
+int main() {
+	int ar[10] ;
+	printf("정수 10개를 입력하시오.\n");
+	for (int i = 0; i <= 9; i++) {
+		scanf("%d", &ar[i]);
+	}
+	Printcount(count_even(ar)); //함수 호출
+	return 0;
+}
+
+//함수 정의
+int count_even(int a[10]) {
+	int cnt=0;
+	for (int i = 0; i < 10; i++) {
+		if (a[i] % 2 == 0) {
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+//함수정의
+void Printcount(int a) {
+	printf("짝수의 개수: %d", a);
+}
